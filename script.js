019f9fd0e3e43e6ea94cc2f183d3c618ae8cec39
Mario Lisbona <mario.lisbona@gmail.com>
2022-07-18 19:10:54 -0500

    Provide success message for valid email

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const email = document.querySelector('.email');
 const submit = document.querySelector('.btn-submit')
 const errorContainer = document.querySelector('.error-container');
 const errorMsg = document.querySelector('.error-message');
+const errorIcon = document.querySelector('.error-icon');
 
 const emailRegex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
 
@@ -22,14 +23,18 @@ submit.addEventListener('click', function (event)  {
 function checkEmail (userInput, regex) {
     if (userInput.match(regex)) {
         email.style.outline = '1px solid var(--desat-red)'; 
-        errorContainer.classList.add('hidden');
+        errorContainer.classList.remove('hidden');
+        errorMsg.textContent = 'Email sent successfuly!';
+        errorIcon.classList.add('hidden');
     } else if (userInput == '') {
         email.style.outline = '1px solid red';
         errorContainer.classList.remove('hidden');
+        errorIcon.classList.remove('hidden');
         errorMsg.textContent = 'Email cannot be empty';   
     } else {
         email.style.outline = '1px solid red';
         errorContainer.classList.remove('hidden');
+        errorIcon.classList.remove('hidden');
         errorMsg.textContent = 'Please provide a valid email';
     }
 };
